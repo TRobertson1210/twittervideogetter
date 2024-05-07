5fd7149093e2e0f4ad43952650459367eb2b9f84
TRobertson1210 <trobertson1210@gmail.com>
2024-05-07 11:07:36 -0400

    update to add copy function

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function main(){
-    // I don't know, just paste this into a bookmarklet
+    // I don't know, just paste this into a bookmarklet. options to open in new tab or copy link. This is because something is making it so I can't right click on the video lel
     javascript:window.open(document.querySelectorAll('video').item(0).src);window.opener.location.reload(false);window.opener.location.replace();
-    
+    javascript:navigator.clipboard.writeText(document.querySelectorAll('video').item(0).src);window.opener.location.reload(false);window.opener.location.replace();
 }
 
