b8098233407922f3d77977eb68d2198c55c069ba
TRobertson1210 <trobertson1210@gmail.com>
2024-05-07 11:01:18 -0400

    script

---
+++ main.ts
@@ -0,0 +1,6 @@
+function main(){
+    // I don't know, just paste this into a bookmarklet
+    javascript:window.open(document.querySelectorAll('video').item(0).src);window.opener.location.reload(false);window.opener.location.replace();
+    
+}
+
